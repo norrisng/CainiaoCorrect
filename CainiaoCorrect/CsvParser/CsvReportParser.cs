74850a24380538d52a12a8d791a40d4ea4ef549d
Norris Ng <c.norrisng@gmail.com>
2017-04-06 15:51:24 +0800

    Groundwork for refactoring CSV report parsing into separate class

---
+++ CainiaoCorrect/CsvParser/CsvReportParser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinyCsvParser;
+
+namespace CainiaoCorrect.CsvParser
+{
+	class CsvReportParser
+	{
+		private List<Shipment> shipments;
+
+		public CsvReportParser(string filename)
+		{
+			parse(filename);
+		}
+
+		private void parse(string filename)
+		{
+			CsvParserOptions csvParserOptions = new CsvParserOptions(true, new[] { ',' });
+			CsvShipmentMapping csvMapper = new CsvShipmentMapping();
+			CsvParser<Shipment> csvParser = new CsvParser<Shipment>(csvParserOptions, csvMapper);
+
+			var results = csvParser.ReadFromFile(@filename, Encoding.UTF8).ToList();
+
+			// manually convert from var
+			for (int i = 0; i < shipments.Count; i++)
+			{
+				shipments.Add(results[i].Result);
+			}
+		}
+
+		public List<Shipment> getShipments()
+		{
+			return shipments;
+		}
+	}
+}
