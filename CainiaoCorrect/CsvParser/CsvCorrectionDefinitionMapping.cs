ebe7bb8f7f3110786c6acae2dae2f753587daea9
Norris Ng <6787487+norrisng@users.noreply.github.com>
2017-04-06 10:53:17 +0800

    Fixed gitignore to not exclude CsvParser

---
+++ CainiaoCorrect/CsvParser/CsvCorrectionDefinitionMapping.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinyCsvParser;
+using CainiaoCorrect.ErrorCorrect;
+
+namespace CainiaoCorrect.CsvParser
+{
+	/// <summary>
+	/// Defines the mapping between the ruleset CSV and the CorrectionDefinition class
+	/// </summary>
+	public class CsvCorrectionDefinitionMapping : TinyCsvParser.Mapping.CsvMapping<CorrectionDefinition>
+	{
+		public CsvCorrectionDefinitionMapping()
+			: base()
+		{
+			MapProperty(0, x => x.errorRegex);
+			MapProperty(1, x => x.correctString);
+		}
+	}
+}
---
+++ CainiaoCorrect/CsvParser/CsvShipmentMapping.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinyCsvParser;
+
+namespace CainiaoCorrect
+{
+	/// <summary>
+	/// Defines the mapping between the Shipment Report CSV and the Shipment class.
+	/// </summary>
+	public class CsvShipmentMapping : TinyCsvParser.Mapping.CsvMapping<Shipment>
+	{
+		public CsvShipmentMapping()
+			: base()
+		{
+			MapProperty(0, x => x.requestTime);
+			MapProperty(1, x => x.shipmentId);
+			MapProperty(2, x => x.status);
+			MapProperty(3, x => x.errorCode);
+			MapProperty(4, x => x.errorMsg);
+			MapProperty(5, x => x.requestFileName);
+			MapProperty(6, x => x.requestFileContent);
+			MapProperty(7, x => x.key);
+		}
+	}
+}
