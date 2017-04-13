bc64bc268a354211f28120cbcd0fbcb4c822a851
Norris Ng <6787487+norrisng@users.noreply.github.com>
2017-04-13 17:00:15 +0800

    Refactor correctPhone(), also fixed critical bug where xml string wasn't loaded in correctPhone()

---
+++ CainiaoCorrect/ErrorCorrect/XmlEditor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CainiaoCorrect.ErrorCorrect
+{
+	class XmlEditor
+	{
+		
+		public XmlEditor()
+		{
+
+		}
+
+		public string replaceXml(string xml, string path, string original, string replacement)
+		{
+			XmlDocument xmlDoc = new XmlDocument();
+			xmlDoc.LoadXml(xml);
+
+			XmlNode node = xmlDoc.SelectSingleNode(path);
+			string originalNode = node.InnerText;
+			node.InnerText = originalNode.Replace(original, replacement);
+
+			return xmlDoc.OuterXml;
+		}
+
+	}
+}
